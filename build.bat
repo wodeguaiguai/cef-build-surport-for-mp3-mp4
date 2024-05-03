5940163c966a116dd635b20533258898d982849e
gsdedebug <gsdedebug@gmail.com>
2024-05-03 08:55:05 +0800

    update

--- build.bat
+++ build.bat
@@ -4,7 +4,6 @@ cd /d %~dp0
 
 set DOWNLOAD_DIR=%~dp0\chromium_git
 set DEPOT_TOOLS_DIR=%~dp0\depot_tools
-@REM set DEPOT_TOOLS_DIR=D:\Environment\depot_tools
 
 set vs_root=C:\Program Files\Microsoft Visual Studio\2022\Professional
 set sdk_root=C:\Program Files (x86)\Windows Kits\10
@@ -15,7 +14,7 @@ set vc_redist_crt=Microsoft.VC143.CRT
 set arch=x64
 
 set CEF_USE_GN=1
-set GN_DEFINES=is_official_build=true is_component_build=true ffmpeg_branding=Chrome proprietary_codecs=true chrome_pgo_phase=0
+set GN_DEFINES=is_official_build=true is_component_build=true ffmpeg_branding=Chrome proprietary_codecs=true chrome_pgo_phase=0 enable_sandbox=false
 set GYP_DEFINES=buildtype=Official
 set GYP_MSVS_VERSION=2022
 set CEF_ARCHIVE_FORMAT=tar.bz2
@@ -34,7 +33,7 @@ set INCLUDE=%sdk_root%\Include\%sdk_version%\um;%sdk_root%\Include\%sdk_version%
 set LIB=%sdk_root%\Lib\%sdk_version%\um\%arch%;%sdk_root%\Lib\%sdk_version\ucrt\%arch%;%vs_root%\VC\Tools\MSVC\%vc_tools_version%\lib\%arch%;%vs_root%\VC\Tools\MSVC\%vc_tools_version%\atlmfc\lib\%arch%;%LIB%
 
 @REM master build
-@REM python automate-git.py --download-dir=%DOWNLOAD_DIR% --depot-tools-dir=%DEPOT_TOOLS_DIR% --no-update --no-debug-build --build-log-file --force-distrib --minimal-distrib --client-distrib --verbose-build --force-build --%arch%-build
+@REM python automate-git.py --download-dir=%DOWNLOAD_DIR% --depot-tools-dir=%DEPOT_TOOLS_DIR% --no-update --no-debug-build --build-log-file --force-distrib --minimal-distrib --client-distrib --verbose-build --%arch%-build
 
 @REM branch build
-python automate-git.py --download-dir=%DOWNLOAD_DIR% --depot-tools-dir=%DEPOT_TOOLS_DIR% --no-update --no-debug-build --build-log-file --force-distrib --minimal-distrib --client-distrib  --verbose-build --force-build --%arch%-build --branch=6422
+python automate-git.py --download-dir=%DOWNLOAD_DIR% --depot-tools-dir=%DEPOT_TOOLS_DIR% --no-update --no-debug-build --build-log-file --force-distrib --minimal-distrib --client-distrib  --verbose-build --%arch%-build --branch=6422
